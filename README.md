2eccb4cf6889b9972ad752de829bf49ee9ed4c4b
andjela-pajic <andjela.pajic90@gmail.com>
2019-09-04 21:57:02 +0200

    init commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Fis job application
+
+Simple test created as an application for a job in Fis.
+
+## Prerequisites
+Python and selenium with webdrivers are required.
+
+## How to run 
+Run "python signInAndApply.py" from command line to start test
---
+++ signInAndApply.py
@@ -0,0 +1,96 @@
+import os
+import html
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.support import expected_conditions as EC
+
+password = 'fisapliciranje'
+email = 'andjela.pajic90@gmail.com'
+
+def waitForElement(driver, time, method, elementName):
+  try:
+    element = WebDriverWait(driver, time).until(
+      EC.presence_of_element_located((method, elementName))
+    )
+  except Exception as e:
+    print('element not found' + elementName)
+    print(e)
+  return element
+# scroll page to element
+def moveToElementByXpath(actions, elementXpath):
+  element = driver.find_element_by_xpath(elementXpath)
+  actions.move_to_element(element).perform()
+
+def pressNext():
+   #wait for the next page and click next
+   waitForElement(driver, 10, By.ID, 'nextButton').click()
+
+try:
+  driver = webdriver.Chrome()
+  driver.maximize_window()
+  driver.get("https://www.fisglobal.com/")
+  actions = ActionChains(driver);
+  #clicking on careers
+  driver.find_element_by_xpath('/html/body/div[2]/header/div/div/div[2]/ul[1]/li[4]').click()
+
+  #hover over menu to select search job
+  moveToElementByXpath(actions, '/html/body/div[2]/header/div/div/div[6]/ul/li[6]/a')
+  driver.find_element_by_xpath('/html/body/div[2]/header/div/div/div[6]/ul/li[6]/a/h3').click()
+  driver.find_element_by_xpath('//*[@id="btnContinue"]').click()
+
+  #clicking on search jobs
+  driver.find_element_by_xpath('//*[@id="494d8f54-3675-480f-b32f-25b818d09861"]/div/div[2]/div[1]/a/div/div/span[2]/span[2]').click()
+  #wait for the search jobs page
+  searchIframe = waitForElement(driver, 10, By.XPATH, '//*[@id="careersframe"]')
+
+  #switch context to the iframe
+  driver.switch_to_frame(searchIframe)
+  #filling the button search Job by ID
+  driver.find_element_by_xpath('//*[@id="com.peopleclick.cp.formdata.SEARCHCRITERIA_CLIENTREQID"]').send_keys('JR71455')
+  #clicking to Search Button
+  driver.find_element_by_id('sp-searchButton').click()
+
+  #waiting for searchApplyButton
+  searchApplyButton = waitForElement(driver, 10, By.ID, 'applybutton_149750_en-us')
+
+  #apply for the position
+  searchApplyButton.click()
+  #filling the input for email
+  driver.find_element_by_xpath('//*[@id="com.peopleclick.cp.formdata.USER_AUTH_PRIMARYEMAILADDRESS"]').send_keys(email)
+  #filling the input for password
+  driver.find_element_by_xpath('//*[@id="com.peopleclick.cp.formdata.USER_AUTH_PASSWORD"]').send_keys(password)
+  #clicking on signInButton
+  driver.find_element_by_xpath('//*[@id="signinButton"]').click()
+
+  #wait for the next page and click next
+  pressNext()
+
+  #wait for the next page and click next
+  pressNext()
+
+  #wait for the RESUME page
+  uploadResumeField = waitForElement(driver, 10, By.XPATH, '//*[@id="uploadRadio"]')
+
+  #upload resume
+  uploadResumeField.click()
+
+  uploadCVField = driver.find_element_by_id('uploadRadioSelectedFile')
+
+  cvPath = "Andjela_Pajic_CV.pdf";
+  driver.execute_script("arguments[0].innerHTML = '" + cvPath + "'", uploadCVField)
+
+  #check radio button for uploading cover letter
+  driver.find_element_by_xpath('//*[@id="uploadCLRadio"]').click()
+  coverLetterPath = "Andjela_Pajic_cover_letter.pdf";
+  #upload cover letter
+  uploadCoverLetterField = driver.find_element_by_id('uploadCLRadioSelectedFile')
+  driver.execute_script("arguments[0].innerHTML = '" + coverLetterPath + "'", uploadCoverLetterField)
+
+  #wait for the next page and click next
+  pressNext()
+
+except Exception as e:
+  print('Something goes wrong')
+  print(e)
